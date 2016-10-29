114ddc0683bfc8d5adac0c22c1f16055a5aaccb0
Ben Bruscella <benbruscella@gmail.com>
2016-10-29 15:01:15 +1100

    3 - Setting up ES6 with Babel and Gulp

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const str = 'ES6';
+console.log(`Hello ${str}`);
+
+let x = 7;
+console.log(x);
+
+class Name {
+    first() {
+        return 'ben';
+    }
+}
+
+let name = new Name()
+
+console.log(name.first())
